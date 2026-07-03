e7d3f9c82cc988e7d620489e4a46cced08cc0f2b
kandula-swathi-sri <161856817+kandula-swathi-sri@users.noreply.github.com>
2026-07-03 22:24:11 +0530

    42_Trapping_Rain_Water.py

---
+++ Two_Pointers/42_Trapping_Rain_Water.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def trap(self, height):
+        l = 0
+        r = len(height) - 1
+
+        left_max = 0
+        right_max = 0
+        water = 0
+
+        while l < r:
+            if height[l] < height[r]:
+                left_max = max(left_max, height[l])
+                water += left_max - height[l]
+                l += 1
+            else:
+                right_max = max(right_max, height[r])
+                water += right_max - height[r]
+                r -= 1
+
+        return water
