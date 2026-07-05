61a0e0fce2ae6cbbfbd6ef9b82ac2473b374b6f9
kandula-swathi-sri <161856817+kandula-swathi-sri@users.noreply.github.com>
2026-07-05 19:41:43 +0530

    gfg_Minimum_Number_Of_Coins.py

---
+++ Greedy/gfg_Minimum_Number_Of_Coins.py
@@ -0,0 +1,10 @@
+class Solution:
+    def findMin(self, n):
+        coins = [10, 5, 2, 1]
+        count = 0
+
+        for coin in coins:
+            count += n // coin
+            n = n % coin
+
+        return count
