e93f49e73d95239e59b0bc95ab103e61b2bdab58
kandula-swathi-sri <161856817+kandula-swathi-sri@users.noreply.github.com>
2026-07-09 19:57:55 +0530

    211_design_add_and_search_words_data_structure.py

---
+++ Trie/211_design_add_and_search_words_data_structure.py
@@ -0,0 +1,43 @@
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.end = False
+
+
+class WordDictionary:
+
+    def __init__(self):
+        self.root = TrieNode()
+
+    def addWord(self, word):
+        node = self.root
+
+        for ch in word:
+            if ch not in node.children:
+                node.children[ch] = TrieNode()
+
+            node = node.children[ch]
+
+        node.end = True
+
+    def search(self, word):
+
+        def dfs(j, node):
+
+            for i in range(j, len(word)):
+                ch = word[i]
+
+                if ch == ".":
+                    for child in node.children.values():
+                        if dfs(i + 1, child):
+                            return True
+                    return False
+
+                if ch not in node.children:
+                    return False
+
+                node = node.children[ch]
+
+            return node.end
+
+        return dfs(0, self.root)
